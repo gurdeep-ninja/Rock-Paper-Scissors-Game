9289c18d7ba7490e194d86cf65b7ecc3897a98da
Gurdeep Bangar <gurdeepbangar@protonmail.com>
2022-12-18 16:41:17 +0000

    Testing game paramaters

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@
 // valid choices for game
 let options = ['rock', 'paper', 'scissors']
 
-// computer picks a random choice from the options
-let computerChoice = options[Math.floor(Math.random() * options.length)]
 
 let userWins = 0
 let computerWins = 0
@@ -12,35 +10,53 @@ let draws = 0
 let gameCount = 0
 let playGame = true
 
-console.log(computerChoice)
-
-/*let userChoice = prompt(`Pick either ${options.toString()}`).toLowerCase().trim()*/
 
 do {
+// computer picks a random choice from the options
+let computerChoice = options[Math.floor(Math.random() * options.length)]    
     let userChoice = 'rock'
 
     if (options.includes(userChoice)) {
 
-        if (userChoice === computerChoice) {
-            console.log("draw")
-        } else if (userChoice == 'rock') {
-
-        }
-        console.log("valid choice")
+        console.log(calculateWin(computerChoice, userChoice))
+        console.log("userWins: "+userWins)
+        console.log("computerWins: "+computerWins)
+        console.log("draws: "+draws)
+        console.log("gameCount:"+gameCount)
     } else {
         console.log("invalid choice")
     }
+    if(gameCount === 10){
+        alert("User wins = " + userWins +
+            " User Losses = " + computerWins +
+            " Draws = " + draws);
+            if(confirm('Play again?')){
+                userWins = 0
+                computerWins = 0
+                draws = 0
+                gameCount = 0
+
+            } else {
+                playGame = false
+            }
+    }
+
+
 } while (playGame === true)
 
 function calculateWin(computerChoice, userChoice) {
+    gameCount++
     if (userChoice === computerChoice) {
+        draws++
         return 'draw'
     } else if (
         (userChoice == 'rock' && computerChoice == 'scissors') ||
         (userChoice == 'scissors' && computerChoice == 'paper') ||
         (userChoice == 'paper' && computerChoice == 'rock')) {
+        userWins++
         return 'win'
     } else {
+        computerWins++
         return 'lose'
     }
 }
